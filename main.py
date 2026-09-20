3db0c11e526071f3ec370d2d064d5a20914f9fab
naren-07-rd <narenthiranath@gmail.com>
2026-09-20 22:58:10 +0530

    Allow production frontend CORS

--- main.py
+++ main.py
@@ -161,11 +161,12 @@ def logout(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:8080",
-    "http://localhost:8080",
-],
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
+        "https://nilamguru.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
